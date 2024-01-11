4255745072bffde95606afb7528d74ed62f2aa15
ndullagh <113929865+ndullagh@users.noreply.github.com>
2024-01-11 15:41:28 -0800

    Step 10

--- packages/react-frontend/src/Table.js
+++ packages/react-frontend/src/Table.js
@@ -1,35 +1,46 @@
 // src/Table.js
-import React from "react";
-
-function Table() {
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Job</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Charlie</td>
-          <td>Janitor</td>
-        </tr>
-        <tr>
-          <td>Mac</td>
-          <td>Bouncer</td>
-        </tr>
-        <tr>
-          <td>Dee</td>
-          <td>Aspiring actress</td>
-        </tr>
-        <tr>
-          <td>Dennis</td>
-          <td>Bartender</td>
-        </tr>
-      </tbody>
-    </table>
+function TableHeader() { 
+    return (
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Job</th>
+            <th>Remove</th>
+          </tr>
+        </thead>
+      );
+}
+function TableBody(props) {
+  const rows = props.characterData.map((row, index) => {
+    return (
+      <tr key={index}>
+        <td>{row.name}</td>
+        <td>{row.job}</td>
+        <td>
+          <button onClick={() => props.removeCharacter(index)}>
+            Delete
+          </button>
+        </td>
+      </tr>
+    );
+   }
   );
+  return (
+      <tbody>
+        {rows}
+       </tbody>
+   );
 }
 
+function Table(props) {
+    return (
+        <table>
+          <TableHeader />
+          <TableBody 
+            characterData={props.characterData} 
+            removeCharacter={props.removeCharacter}/>
+
+        </table>
+      );
+}
 export default Table;
